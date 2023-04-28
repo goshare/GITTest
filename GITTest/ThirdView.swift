199197e2fd9396b865f4c5e9e9d327f82ba2ede0
Jaspreet singh <jaspalsingh634@gmail.com>
2023-04-28 18:06:21 +0530

    my new version of the app

---
+++ GITTest/ThirdView.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdView.swift
+//  GITTest
+//
+//  Created by jaspreet on 28/04/23.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        VStack{
+            Text("hi")
+        }.font(.title)
+    }
+}
+
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdView()
+    }
+}
