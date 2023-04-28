e95851b96cb6852354aa3d213f26987a4a254935
Jaspreet singh <jaspalsingh634@gmail.com>
2023-04-28 17:56:21 +0530

    Initial Commit

---
+++ GITTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GITTest
+//
+//  Created by jaspreet on 28/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GITTest/GITTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITTestApp.swift
+//  GITTest
+//
+//  Created by jaspreet on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct GITTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
